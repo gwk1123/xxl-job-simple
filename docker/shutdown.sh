b87b6e14d8dcf6cdaa6387d3c1ca49463b1c3fd7
gwk1123 <g15625281551@163.com>
2021-01-21 21:41:47 +0800

    first commit

---
+++ docker/shutdown.sh
@@ -0,0 +1,4 @@
+APPNAME=xxljobsimple
+docker stop $APPNAME
+docker rm $APPNAME
+docker rmi $APPNAME
---
+++ src/main/java/com/xxl/job/admin/XxlJobSimpleApplication.java
@@ -0,0 +1,13 @@
+package com.xxl.job.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobSimpleApplication.class, args);
+    }
+
+}

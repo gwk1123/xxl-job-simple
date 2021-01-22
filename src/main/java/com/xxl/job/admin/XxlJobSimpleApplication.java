91dd46367a7ee41e42db6943c90625b8f17da78f
gwk1123 <g15625281551@163.com>
2021-01-22 10:28:15 +0800

    docker

--- src/main/java/com/xxl/job/admin/XxlJobSimpleApplication.java
+++ src/main/java/com/xxl/job/admin/XxlJobSimpleApplication.java
@@ -2,7 +2,9 @@ package com.xxl.job.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class XxlJobSimpleApplication {
 

c1b471875815e8da92786001b2febab7732ae268
gwk1123 <g15625281551@163.com>
2022-01-30 18:38:56 +0800

    更改docker为默认模式 修改Docker时区-否则里面的时间会慢8小时 通过文件链接的方式修改 时区

--- src/main/java/com/xxl/job/admin/XxlJobSimpleApplication.java
+++ src/main/java/com/xxl/job/admin/XxlJobSimpleApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+/**
+ * docker部署xxl-job镜像，启动成功却无法访问 (编辑这个目录下的文件 /usr/lib/sysctl.d/00-system.conf )
+ * https://blog.csdn.net/qq_40397586/article/details/120783718
+ * https://www.cnblogs.com/jinanxiaolaohu/p/8309690.html
+ */
+
 @EnableEurekaClient
 @SpringBootApplication
 public class XxlJobSimpleApplication {
